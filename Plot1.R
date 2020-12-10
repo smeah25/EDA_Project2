05351dadb9c80b9c0d4ff0953aaff13b580f8d18
smeah25 <smeah25@gmail.com>
2020-12-10 22:26:00 +0800

    Course Project 2: Plot 1

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+# Checking directory for data
+if(!exists("NEI")){
+  NEI <- readRDS("./summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./Source_Classification_Code.rds")
+}
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+# Creating PNG file and Plotting Data
+png('Plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+
+# Closing PNG file
+dev.off()
